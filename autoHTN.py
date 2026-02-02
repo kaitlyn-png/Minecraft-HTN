f01c65a99378540cc26c5db57b05fd724cc1de0e
Kaitlyn Eng <123908591+kaitlyn-png@users.noreply.github.com>
2026-02-02 14:59:48 -0800

    first 3 test cases works

--- autoHTN.py
+++ autoHTN.py
@@ -17,51 +17,120 @@ pyhop.declare_methods('produce', produce)
 
 def make_method(name, rule):
 	def method(state, ID):
-		# your code here
-		pass
+		tasks = []
 
+		for tool in rule.get('Requires', {}):
+			tasks.append(('have_enough', ID, tool, 1))
+
+		for item, qty in rule.get('Consumes', {}).items():
+			tasks.append(('have_enough', ID, item, qty))
+
+		tasks.append((name, ID))
+
+		return tasks
 	return method
+
+# RANK TOOL
+
+def tool_rank(req):
+    if not req:
+        return 0
+    if 'wooden_pickaxe' in req:
+        return 1
+    if 'stone_pickaxe' in req:
+        return 2
+    if 'iron_pickaxe' in req:
+        return 3
+    return 4
 
 def declare_methods(data):
 	# some recipes are faster than others for the same product even though they might require extra tools
 	# sort the recipes so that faster recipes go first
 
 	# your code here
-	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)	
-	pass			
+	# hint: call make_method, then declare the method to pyhop using pyhop.declare_methods('foo', m1, m2, ..., mk)
+
+	recipes_by_product = {}
+
+	for name, rule in data['Recipes'].items():
+		for product in rule['Produces']:
+			recipes_by_product.setdefault(product, []).append((name, rule))
+
+	for product, rules in recipes_by_product.items():
+		# (key=lambda x: x[1]['Time'])
+		rules.sort(key=lambda x: (
+			tool_rank(x[1].get('Requires')),
+            x[1]['Time']
+		))
+
+
+		methods = []
+		for name, rule in rules:
+			op_name = f"op_{name}"
+			methods.append(make_method(op_name, rule))
+
+		pyhop.declare_methods(f'produce_{product}', *methods)
+		# debugging
+		#pyhop.print_methods()
 
 def make_operator(rule):
 	def operator(state, ID):
-		# check time
+
 		if state.time[ID] < rule['Time']:
 			return False
-		
-		# check inputs
+
 		for item, qty in rule.get('Consumes', {}).items():
 			if getattr(state, item)[ID] < qty:
 				return False
-			
-		# apply consumes
-		for item, qty in rule.get('Produce', {}).items():
-			if getattr(state, item)[ID] < qty:
-				return False
-		
+
+		for item, qty in rule.get('Consumes', {}).items():
+			getattr(state, item)[ID] -= qty
+
+		for item, qty in rule.get('Produces', {}).items():
+			getattr(state, item)[ID] += qty
+
 		state.time[ID] -= rule['Time']
 		return state
+
 	return operator
+
 
 def declare_operators(data):
 	# your code here
 	# hint: call make_operator, then declare the operator to pyhop using pyhop.declare_operators(o1, o2, ..., ok)
-	pass
+	ops = []
+
+	for name, rule in data['Recipes'].items():
+		op = make_operator(rule)
+		op.__name__ = f"op_{name}"
+		ops.append(op)
+
+	pyhop.declare_operators(*ops)
 
 def add_heuristic(data, ID):
 	# prune search branch if heuristic() returns True
 	# do not change parameters to heuristic(), but can add more heuristic functions with the same parameters: 
 	# e.g. def heuristic2(...); pyhop.add_check(heuristic2)
 	def heuristic(state, curr_task, tasks, plan, depth, calling_stack):
-		# your code here
-		return False # if True, prune this branch
+		
+		# prune impossible time
+		if state.time[ID] < 0:
+			return True
+
+	 	# prevent recursive production loops
+		if curr_task[0].startswith('produce_'):
+			item = curr_task[0][8:]
+
+			for t in calling_stack:
+				if t[0] == curr_task[0]:
+					return True
+		
+		# # block self recursion
+		# if len(calling_stack) > 0:
+		# 	if curr_task == calling_stack[-1]:
+		# 		return True
+			
+		return False
 
 	pyhop.add_check(heuristic)
 
@@ -92,6 +161,9 @@ def set_up_goals(data, ID):
 	goals = []
 	for item, num in data['Problem']['Goal'].items():
 		goals.append(('have_enough', ID, item, num))
+	
+	# # rails first, then carts
+	# goals.sort(key=lambda g: g[2] == 'cart')
 
 	return goals
 
